f4974bb6484eea6ec8c3d5e95cc91529c1c5a573
Steven Sullivan <stevensullivan256@gmail.com>
2022-05-30 15:08:32 -0700

    chore: delete examples folder

--- examples/index.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="react" />
-interface FallbackProps {
-    isLoading: boolean;
-    children: JSX.Element | JSX.Element[];
-    fallbackOnStaticContent?: boolean;
-}
-export default function Fallback({ isLoading, children, fallbackOnStaticContent }: FallbackProps): JSX.Element;
-export {};
--- examples/useFallback.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="react" />
-interface UseFallbackProps {
-    isLoading: boolean;
-    fallbackOnStaticContent?: boolean;
-}
-export default function useFallback({ isLoading, fallbackOnStaticContent }: UseFallbackProps): {
-    withFallback: (tree: JSX.Element) => JSX.Element;
-};
-export {};
